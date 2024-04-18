aa75990cc8b97857e53dc00af820842b12826bc6
AlCkoNi <Tolibjon2004@yandex.ru>
2024-04-19 00:33:13 +0500

    Работает полностью функция проверки пароля но выдает ошибку если пароль 0000

--- Program.cs
+++ Program.cs
@@ -12,11 +12,12 @@ namespace Bankamat
             menu.startMenu();
         }
     }
-    public class Language()
+    public class Language// Локализация
     {
         public readonly string[] chose = { " -= Tilni tanlang ", " Виберите язык ", " Chose language =- \n\n " +
-                " -= Uzb = 1 , Ru = 2 , Eng = 3 =- \n" };
+                "        -= Uzb = 1 , Ru = 2 , Eng = 3 =- \n" };
         public readonly string[] passTxt = { " Parolni kiriting : ", " Введите пароль : ", " Enter pssword : " };
+        public readonly string[] passTxtIsNotTrue = { " Parolingiz hato \n", " Введеный пароль неправельно \n", " Entered pssword is not true \n" };
         public readonly string[] welcom = { " -= Hushkelibsiz =-"," -= Добропожаловать =- ", " -= Wellcom =-" };
         public readonly string[] choseMenu = {" Menudan birini tanlang ", " Выберите пункт из меню ", " Chose one punkt in menu "};
         public readonly string[] menuS = {" 1.Hisobingizdi korish \n 2.Naqt pul yechish \n 3.Pul tashlash \n 4.Sms hizmatlari \n 5.Parolni almashtirish",
@@ -25,25 +26,31 @@ namespace Bankamat
     }
     public class Menu : Language
     {
-        public void startMenu()
+        int choseLang;
+        bool folsePasswd = false;
+        public void startMenu() // Меню окно выброра
         {
             metka:
-            for (int i = 0; i < chose.Length; i++)
+            for (int i = 0; i < chose.Length; i++)//Вывод выбора языка
             {
                 Write(chose[i]);
             }
             WriteLine();
-            Write("-= : ");
+            Write(" -= : ");
             try
             {
-                int choseLang = int.Parse(ReadLine()!);
+                choseLang = int.Parse(ReadLine()!);
                 switch (choseLang)
                 {
                     case 1:
                         {
                             Clear();
-                            UZB uZB = new UZB();
-                            uZB.uzb();
+                            enterPasswd();
+                            if(folsePasswd == true)
+                            {
+                                UZB uZB = new UZB();
+                                uZB.uzb();
+                            }
                         }; break;
                     case 2:
                         {
@@ -64,16 +71,55 @@ namespace Bankamat
                 WriteLine(e);
             }
         }
-        public static int passwdForEnter = 0000;
-        public void enterPasswd()
+        public int passwdForEnter = 1111;//Пароль неизменая //Exepation with 0000
+        public void enterPasswd()//Функция проверки пороля
         {
-            Write(passTxt[0]);
+            metka:
+            switch(choseLang)//Приведствие
+            {
+                case 1:
+                    {
+                        Write(passTxt[0]);
+                    }; break;
+                case 2:
+                    {
+                        Write(passTxt[1]);
+                    }; break;
+                case 3:
+                    {
+                        Write(passTxt[1]);
+                    }; break;
+            }
             try
             {
                 int passwdEnter = int.Parse(ReadLine()!);
                 if(passwdEnter == passwdForEnter)
                 {
-
+                    folsePasswd = true;
+                }
+                else//Если не правельно 
+                {
+                    switch (choseLang)
+                    {
+                        case 1:
+                            {
+                                Clear();
+                                WriteLine(passTxtIsNotTrue[0]);
+                                goto metka;
+                            };
+                        case 2:
+                            {
+                                Clear();
+                                WriteLine(passTxtIsNotTrue[1]);
+                                goto metka;
+                            };
+                        case 3:
+                            {
+                                Clear();
+                                WriteLine(passTxtIsNotTrue[2]);
+                                goto metka;
+                            };
+                    }
                 }
             }
             catch(Exception e)
@@ -82,21 +128,21 @@ namespace Bankamat
             }
         }
     }
-    public class UZB : Language
+    public class UZB : Language // Локализация Узбекского языка
     {
         public void uzb()
         {
             Write($"{welcom[0]}\n\n{choseMenu[0]}\n\n{menuS[0]}\n\n -= : ");
         }
     }
-    public class RU : Language
+    public class RU : Language // Локализация Русскаого языка
     {
         public void ru()
         {
             Write($"{welcom[1]}\n\n{choseMenu[1]}\n\n{menuS[1]}\n\n -= : ");
         }
     }
-    public class ENG : Language
+    public class ENG : Language  // Локализация Ангийского языка
     {
         public void eng()
         {

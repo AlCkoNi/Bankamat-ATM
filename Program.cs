7e3b203c57d5b159301a6a81cc61ec1307388fff
AlCkoNi <Tolibjon2004@yandex.ru>
2024-04-18 23:51:48 +0500

    Добавления выбора языка и создание функции пароля

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Channels;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+using static System.Console;
 
 namespace Bankamat
 {
@@ -14,7 +16,12 @@ namespace Bankamat
     {
         public readonly string[] chose = { " -= Tilni tanlang ", " Виберите язык ", " Chose language =- \n\n " +
                 " -= Uzb = 1 , Ru = 2 , Eng = 3 =- \n" };
+        public readonly string[] passTxt = { " Parolni kiriting : ", " Введите пароль : ", " Enter pssword : " };
         public readonly string[] welcom = { " -= Hushkelibsiz =-"," -= Добропожаловать =- ", " -= Wellcom =-" };
+        public readonly string[] choseMenu = {" Menudan birini tanlang ", " Выберите пункт из меню ", " Chose one punkt in menu "};
+        public readonly string[] menuS = {" 1.Hisobingizdi korish \n 2.Naqt pul yechish \n 3.Pul tashlash \n 4.Sms hizmatlari \n 5.Parolni almashtirish",
+                                          " 1.Посмотреть баланс \n 2.Выдача наличных \n 3.Отправка средств \n 4.СМС услуги \n 5.Смена пороля",
+                                          " 1.Check your balans \n 2.Givs nul maney \n 3.Pay money \n 4.Sms function \n 5.Update your password"};
     }
     public class Menu : Language
     {
@@ -23,34 +30,77 @@ namespace Bankamat
             metka:
             for (int i = 0; i < chose.Length; i++)
             {
-                Console.Write(chose[i]);
+                Write(chose[i]);
             }
-            Console.WriteLine();
-            Console.Write("-= Tanlovingiz : ");
+            WriteLine();
+            Write("-= : ");
             try
             {
-                int choseLang = int.Parse(Console.ReadLine()!);
+                int choseLang = int.Parse(ReadLine()!);
                 switch (choseLang)
                 {
                     case 1:
                         {
-                            Console.WriteLine(welcom[0]);
+                            Clear();
+                            UZB uZB = new UZB();
+                            uZB.uzb();
                         }; break;
                     case 2:
                         {
-                            Console.WriteLine(welcom[1]);
+                            Clear();
+                            RU rU = new RU();
+                            rU.ru();
                         }; break;
                     case 3:
                         {
-                            Console.WriteLine(welcom[2]);
+                            Clear();
+                            ENG eNG = new ENG();
+                            eNG.eng();
                         }; break;
-                    default: goto metka;
+                    default: Clear(); goto metka;
                 }
             }catch (Exception e)
             {
-                Console.WriteLine(e);
+                WriteLine(e);
+            }
+        }
+        public static int passwdForEnter = 0000;
+        public void enterPasswd()
+        {
+            Write(passTxt[0]);
+            try
+            {
+                int passwdEnter = int.Parse(ReadLine()!);
+                if(passwdEnter == passwdForEnter)
+                {
+
+                }
+            }
+            catch(Exception e)
+            {
+                WriteLine(e);
             }
         }
     }
-    
+    public class UZB : Language
+    {
+        public void uzb()
+        {
+            Write($"{welcom[0]}\n\n{choseMenu[0]}\n\n{menuS[0]}\n\n -= : ");
+        }
+    }
+    public class RU : Language
+    {
+        public void ru()
+        {
+            Write($"{welcom[1]}\n\n{choseMenu[1]}\n\n{menuS[1]}\n\n -= : ");
+        }
+    }
+    public class ENG : Language
+    {
+        public void eng()
+        {
+            Write($"{welcom[2]}\n\n{choseMenu[2]}\n\n{menuS[2]}\n\n -= : ");
+        }
+    }
 }
